b6b125cd2a5dfbe9eb6a22602a4e59383fad8fe8
zhouxiaomin <Z564509>
2019-07-22 09:39:36 +0800

    写死复制war包的命令,全部tomcat的文件路径

--- deploy.sh
+++ deploy.sh
@@ -27,17 +27,18 @@ mvn clean install
 killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -rf /root/tomcat/webapps/ROOT
+rm -f /root/tomcat/webapps/ROOT.war
+rm -f /root/tomcat/webapps/order.war
 
 # 复制新的工程
 #cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 cp /root/.jenkins/workspace/DeployOrder/order/target/order.war /root/tomcat/webapps/
-cd $TOMCAT_APP_PATH/webapps/
+#cd $TOMCAT_APP_PATH/webapps/
+cd /root/tomcat/webapps/
 mv order.war ROOT.war
 # 启动Tomcat
-cd $TOMCAT_APP_PATH/
+cd /root/tomcat/
 sh bin/startup.sh
 echo $PROJ_PATH
 echo "$PROJ_PATH"
