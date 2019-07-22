811e096eb5268082ee0d6a120c1a02895a1292a8
zhouxiaomin <Z564509>
2019-07-22 09:16:29 +0800

    输出deploy.sh的成功标识

--- deploy.sh
+++ deploy.sh
@@ -32,14 +32,13 @@ rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
-#cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-echo "11111111111111111111"
+cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
-echo "22222222222222222222"
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
 sh bin/startup.sh
 
-echo "over"
+echo "over  >>>>>  脚本全部执行完成！"
 
