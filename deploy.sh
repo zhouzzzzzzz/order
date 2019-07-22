c8c00ba223f873fdff7fabeafd2185edb975350f
zhouxiaomin <Z564509>
2019-07-22 09:13:39 +0800

    修改deploy.sh脚本

--- deploy.sh
+++ deploy.sh
@@ -33,13 +33,13 @@ rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
 #cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
+echo "11111111111111111111"
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
-
+echo "22222222222222222222"
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
 sh bin/startup.sh
 
-
+echo "over"
 
